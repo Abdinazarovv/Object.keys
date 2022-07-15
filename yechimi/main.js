8ffc0d0260804a8211e5069b2825c8959abd0ae7
Abdinazarovv <islombekabdinazarov07@gmail.com>
2022-07-15 09:35:23 +0300

    first commit

---
+++ yechimi/main.js
@@ -0,0 +1,14 @@
+let phoneNumber = {
+    user1: 1111111,
+    user2: 2222222,
+    user3: 3333333,
+    user4: 4444444,
+    user5: 5555555,
+    user6: 6666666,
+    user7: 7777777
+}
+
+var userKeys = Object.keys(phoneNumber);
+console.log(userKeys);
+
+// keylardan tuzilgan arraylarni string ko'rinishida qaytarib beradi
